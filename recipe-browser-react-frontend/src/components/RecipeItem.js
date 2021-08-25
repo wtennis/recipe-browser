fa8e5f34986a533f14054f25d3d4f1ec8fcf0d67
Whiting <whitinghtennis@gmail.com>
2021-08-25 14:22:52 -0700

    recipe/:id/ingredients route setup

--- recipe-browser-react-frontend/src/components/RecipeItem.js
+++ recipe-browser-react-frontend/src/components/RecipeItem.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RecipeItem({ recipe }){
+    const [ingredients, setIngredients] = useState([])
 
-function handleDeleteClick() {
-    console.log('delete click')
-}
+    function handleConsoleOneRecipeClick() {
+        fetch(`http://localhost:9292/recipes/${recipe.id}`, {
+          method: "GET",
+        })
+          .then((r) => r.json())
+          .then(console.log);
+      }
+
+
+    function handleIngredientsClick() {
+        console.log('handleIngredientsClick')
+        
+        fetch(`http://localhost:9292/recipes/${recipe.id}/ingredients`)
+            .then((r) => r.json())
+            .then(console.log);
+
+    }
 
 
     return (
         <div>
             <h3>{recipe.name}</h3>
             <p>{recipe.description}</p>
-            <button onClick = {() => handleDeleteClick(recipe.id)}>Delete</button>
+            <button onClick = {() => handleConsoleOneRecipeClick(recipe.id)}>Console.log this recipe</button>
+
+       
+        {ingredients.length === 0?
+            <button onClick = {handleIngredientsClick}>Ingredients</button>
+            :
+            <h4>Ingredients...</h4>
+        }
+
         </div>
     
       );
