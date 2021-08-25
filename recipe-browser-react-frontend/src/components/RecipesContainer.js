f241c55b32b6804cc4f16837c6d6f401553609f1
Whiting <whitinghtennis@gmail.com>
2021-08-25 12:25:14 -0700

    first fetch done

--- recipe-browser-react-frontend/src/components/RecipesContainer.js
+++ recipe-browser-react-frontend/src/components/RecipesContainer.js
@@ -1,12 +1,42 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import RecipeItem from './RecipeItem';
 
 function RecipesContainer(){
+  const [recipes, setRecipes] = useState([])
 
+  const URL = "http://localhost:9292/recipes"
+
+  useEffect(() => {
+    async function fetchRecipes() {
+        const res = await fetch(URL, {
+            headers: { Accept: 'application/json' }
+        })
+
+        const parsedBody = await res.json();
+
+        setRecipes(parsedBody);
+        }
+
+fetchRecipes();
+}, [])
 
   return (
-    <div>This is RecipesContainer</div>
-  );
+    <>
+        {recipes.length !== 0? 
+                <div>
+      
+                <h3>This is RecipesContainer</h3>
+                {recipes.map((recipe) => (
+                        <RecipeItem recipe={recipe}/>
+                    ))}
+                
+                </div>
+            :
+                <h1>Loading...</h1>
+    }
+    </>
+);
+
 }
 
 export default RecipesContainer;
